959219a1f6c3f1d627520fb105352c2bb550cac6
Domia <39hson@gmail.com>
2020-11-10 14:41:29 +0700

    Done "feature" and "adv" sections

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,4 +6,22 @@ function init() {
             item.href = "javascript:;"
     }
 }
+
+function createAccordion() {
+    var acc = document.getElementsByClassName("feature__accordion");
+    var i;
+
+    for (i = 0; i < acc.length; i++) {
+        acc[i].addEventListener("click", function () {
+            this.classList.toggle("feature__accordion--active");
+            var panel = this.nextElementSibling;
+            if (panel.style.display === "block") {
+                panel.style.display = "none";
+            } else {
+                panel.style.display = "block";
+            }
+        });
+    }
+}
 init();
+createAccordion();
