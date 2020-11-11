d781715ea7c1eb3716344c0eb8cd27755d4e3180
Domia <39hson@gmail.com>
2020-11-11 19:52:00 +0700

    Test github page

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,7 +2,7 @@ let a = document.getElementsByTagName("a");
 
 function init() {
     for (let item of a) {
-        if (item.href == "http://127.0.0.1:5500/index.html" || item.href == "")
+        if (item.href == "https://39domia.github.io/uni-corp.github.io/" || item.href == "")
             item.href = "javascript:;"
     }
 }
