e7ea67f878010e2845778d22b835160686786d94
Domia <39hson@gmail.com>
2020-11-16 22:52:01 +0700

    Optimize nar bar table

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,27 +1,39 @@
 let a = document.getElementsByTagName("a");
 
 function init() {
-    for (let item of a) {
-        if (item.href == "https://39domia.github.io/uni-corp/" || item.href == "")
-            item.href = "javascript:;"
-    }
+  for (let item of a) {
+    if (item.href == "https://39domia.github.io/uni-corp/" || item.href == "")
+      item.href = "javascript:;";
+  }
 }
 
 function createAccordion() {
-    var acc = document.getElementsByClassName("feature__accordion");
-    var i;
+  var acc = document.getElementsByClassName("feature__accordion");
+  var i;
 
-    for (i = 0; i < acc.length; i++) {
-        acc[i].addEventListener("click", function () {
-            this.classList.toggle("feature__accordion--active");
-            var panel = this.nextElementSibling;
-            if (panel.style.display === "block") {
-                panel.style.display = "none";
-            } else {
-                panel.style.display = "block";
-            }
-        });
-    }
+  for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function () {
+      this.classList.toggle("feature__accordion--active");
+      var panel = this.nextElementSibling;
+      if (panel.style.display === "block") {
+        panel.style.display = "none";
+      } else {
+        panel.style.display = "block";
+      }
+    });
+  }
 }
-init();
-createAccordion();
+document.getElementById("header__nav-table-list").addEventListener("click", turnOfOverlay);
+function turnOfOverlay(e) {
+  let navCheckbox = document.getElementById("header__nav-checkbox");
+  if (e.target.localName == "a") {
+    navCheckbox.checked = false;
+  }
+}
+
+function documentReady() {
+  init();
+  createAccordion();
+}
+
+documentReady();
